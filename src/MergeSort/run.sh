a87cf8b6d71d31c2831a49e1b4b9a92ce24d6efe
Paul Ofremu Jr <pauljrofremu@gmail.com>
2022-12-08 19:27:29 -0500

    Add parameter for number of threads and adjust tests

--- src/MergeSort/run.sh
+++ src/MergeSort/run.sh
@@ -5,6 +5,7 @@ g++ seq_mergesort.cpp -o seq_mergesort.o
 g++ par_mergesort.cpp -fopenmp -o par_mergesort.o
 
 sizes=(25000 50000 75000 100000 250000 500000 750000 1000000)
+threads=(1 2 4 6)
 
 echo "---Sequential Merge Sort---"
 echo ""
@@ -34,23 +35,30 @@ echo ""
 echo "---Parallel Merge Sort---"
 echo ""
 echo "Random Input:"
-for i in "${sizes[@]}"; do
-  time=$(./par_mergesort.o "$i" "../data/Random/randomArray_$i")
-  echo "size: $i -> $time" 
+
+for j in "${threads[@]}"; do
+  for i in "${sizes[@]}"; do
+    time=$(./par_mergesort.o "$i" "../data/Random/randomArray_$i" "$j")
+    echo "threads: $j; size: $i -> $time" 
+  done
 done
 
 echo ""
 echo "Sorted Input:"
-for i in "${sizes[@]}"; do
-  time=$(./par_mergesort.o "$i" "../data/Sorted/sorted_$i")
-  echo "size: $i -> $time" 
+for j in "${threads[@]}"; do
+  for i in "${sizes[@]}"; do
+    time=$(./par_mergesort.o "$i" "../data/Sorted/sorted_$i" "$j")
+    echo "threads: $j; size: $i -> $time" 
+  done
 done
 
 echo ""
 echo "Reverse Sorted Input:"
-for i in "${sizes[@]}"; do
-  time=$(./par_mergesort.o "$i" "../data/ReverseSorted/reverseSortedArray_$i")
-  echo "size: $i -> $time" 
+for j in "${threads[@]}"; do
+  for i in "${sizes[@]}"; do
+    time=$(./par_mergesort.o "$i" "../data/ReverseSorted/reverseSortedArray_$i" "$j")
+    echo "threads: $j; size: $i -> $time" 
+  done
 done
 
 rm seq_mergesort.o par_mergesort.o
--- src/QuickSort/run.sh
+++ src/QuickSort/run.sh
@@ -5,6 +5,7 @@ g++ SeqQuicksort.cpp -o SeqQuicksort.o
 g++ ParQuicksort.cpp -fopenmp -o ParQuicksort.o
 
 sizes=(25000 50000 75000 100000 250000 500000 750000 1000000)
+threads=(1 2 4 6)
 
 echo "---Sequential QuickSort Sort---"
 echo ""
@@ -17,7 +18,7 @@ done
 echo ""
 echo "Sorted Input:"
 for i in "${sizes[@]}"; do
-  time=$(./SeqQuicksort.o.o "$i" "../data/Sorted/sorted_$i")
+  time=$(./SeqQuicksort.o "$i" "../data/Sorted/sorted_$i")
   echo "size: $i -> $time" 
 done
 
@@ -33,23 +34,29 @@ echo ""
 echo "---Parallel QuickSort---"
 echo ""
 echo "Random Input:"
-for i in "${sizes[@]}"; do
-  time=$(./ParQuicksort.o "$i" "../data/Random/randomArray_$i")
-  echo "size: $i -> $time" 
+for j in "${threads[@]}"; do
+  for i in "${sizes[@]}"; do
+    time=$(./ParQuicksort.o "$i" "../data/Random/randomArray_$i" "$j")
+    echo "threads: $j; size: $i -> $time" 
+  done
 done
 
 echo ""
 echo "Sorted Input:"
-for i in "${sizes[@]}"; do
-  time=$(./ParQuicksort.o "$i" "../data/Sorted/sorted_$i")
-  echo "size: $i -> $time" 
+for j in "${threads[@]}"; do
+  for i in "${sizes[@]}"; do
+    time=$(./ParQuicksort.o "$i" "../data/Sorted/sorted_$i" "$j")
+    echo "threads: $j; size: $i -> $time" 
+  done
 done
 
 echo ""
 echo "Reverse Sorted Input:"
-for i in "${sizes[@]}"; do
-  time=$(./ParQuicksort.o "$i" "../data/ReverseSorted/reverseSortedArray_$i")
-  echo "size: $i -> $time" 
+for j in "${threads[@]}"; do
+  for i in "${sizes[@]}"; do
+    time=$(./ParQuicksort.o "$i" "../data/ReverseSorted/reverseSortedArray_$i" "$j")
+    echo "size: $i -> $time" 
+  done
 done
 
 rm SeqQuicksort.o ParQuicksort.o
